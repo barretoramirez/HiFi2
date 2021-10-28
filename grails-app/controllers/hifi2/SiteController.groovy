4522cb3b0a1fa8d632a737586238752414d64cab
Jorge Barreto <barretoramirez@gmail.com>
2021-10-27 19:42:07 -0500

    FirstCommit

---
+++ grails-app/controllers/hifi2/SiteController.groovy
@@ -0,0 +1,8 @@
+package hifi2
+
+class SiteController {
+
+    def index() { 
+        render "Hello World!"
+    }
+}
---
+++ grails-app/init/hifi2/BootStrap.groovy
@@ -0,0 +1,9 @@
+package hifi2
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
